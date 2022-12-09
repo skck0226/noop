0bd84276c89fce4a261fccb7f5aa5e29e59996d2
skck0226 <skck0226@ajou.ac.kr>
2022-12-09 17:16:54 +0900

    [feat] 프로젝트 생성, mariadb 연동

---
+++ src/main/java/com/ajou/noop/NoopApplication.java
@@ -0,0 +1,13 @@
+package com.ajou.noop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajou/noop/NoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajou.noop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

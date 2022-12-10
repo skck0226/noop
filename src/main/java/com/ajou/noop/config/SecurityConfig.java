67a56544eee22d4bbd6dca2dd6b1a0a3ef282aa9
skck0226 <skck0226@ajou.ac.kr>
2022-12-10 19:08:05 +0900

    [feat] SecurityConfig csrf 설정

--- src/main/java/com/ajou/noop/config/SecurityConfig.java
+++ src/main/java/com/ajou/noop/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.ajou.noop.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -11,7 +13,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
+                .csrf().disable()
                 .formLogin().disable()
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/ajou/noop/user/UserController.java
+++ src/main/java/com/ajou/noop/user/UserController.java
@@ -1,12 +1,19 @@
 package com.ajou.noop.controller;
 
+import com.ajou.noop.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/user")
 public class UserController {
+
     @GetMapping("/")
-    public String hello(){
-        return "hello";
+    public List<User> allUser(){
+
+        return
     }
 }

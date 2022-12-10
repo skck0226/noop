41c38b1e0be8f502b441a1d8ab9d8c888cf3d82f
skck0226 <skck0226@ajou.ac.kr>
2022-12-11 00:41:04 +0900

    [feat] SecurityConfig cors 설정 corsFilter로 변경

--- src/main/java/com/ajou/noop/config/SecurityConfig.java
+++ src/main/java/com/ajou/noop/config/SecurityConfig.java
@@ -1,21 +1,43 @@
 package com.ajou.noop.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
+                .cors()
+                .and()
                 .csrf().disable()
-                .formLogin().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .build();
+    }
+
+//    securityFilterChain에 등록할 cors 설정 필터
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     @Bean
